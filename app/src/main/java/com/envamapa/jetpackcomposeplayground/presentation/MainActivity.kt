c452db104769e296f5c6b92b464d29317ba232bc
Enya Martínez <envamapa@gmail.com>
2021-07-02 11:03:17 -0500

    Add composable function

--- app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
+++ app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             setContent {
-                Text("Hello World!")
+                Greeting(name = "Android")
             }
         }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name")
 }
