8a3e03840ef780b6fc2581eb46adb7b3eae18d30
Enya Martínez <envamapa@gmail.com>
2021-07-02 11:32:54 -0500

    Modify imports based on Ktlint

--- app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
+++ app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
