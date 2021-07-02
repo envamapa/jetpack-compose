ea40bd57666b4581a872ae35bb97e9082bf91e63
Enya Martínez <envamapa@gmail.com>
2021-07-02 11:31:04 -0500

    Add modifiers to image and space between image and text

--- app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
+++ app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,8 +32,13 @@ fun NewsStory() {
     ) {
         Image(
             painter = painterResource(id = R.drawable.header),
-            contentDescription = "Nice header"
+            contentDescription = "Nice header",
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxHeight(),
+            contentScale = ContentScale.Crop
         )
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = "A day in Shark Fin Cove")
         Text(text = "Davenport, California")
         Text(text = "December 2018")
