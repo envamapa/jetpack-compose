365ed7b19a5bf63c611db21762895a9a1ea8a8f7
Enya Martínez <envamapa@gmail.com>
2021-07-02 11:00:22 -0500

    Add text element

--- app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
+++ app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
@@ -3,36 +3,15 @@ package com.envamapa.jetpackcomposeplayground.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.envamapa.jetpackcomposeplayground.presentation.ui.theme.JetpackComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            setContent {
+                Text("Hello World!")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposePlaygroundTheme {
-        Greeting("Android")
     }
 }
