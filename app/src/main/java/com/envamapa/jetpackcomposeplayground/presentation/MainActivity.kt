bb423d2e2fef73c45b83fb38307abd139e108861
Enya Martínez <envamapa@gmail.com>
2021-07-02 13:00:07 -0500

    Set rounded corners to image

--- app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
+++ app/src/main/java/com/envamapa/jetpackcomposeplayground/presentation/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,7 +41,10 @@ fun NewsStory() {
             contentDescription = "Nice header",
             modifier = Modifier
                 .height(180.dp)
-                .fillMaxHeight(),
+                .fillMaxHeight()
+                .clip(
+                    shape = RoundedCornerShape(4.dp)
+                ),
             contentScale = ContentScale.Crop
         )
         Spacer(modifier = Modifier.height(16.dp))
